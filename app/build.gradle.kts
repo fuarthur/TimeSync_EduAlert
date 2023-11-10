c21c212d920c0539068f36df3ba5d3a47b2fe36b
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2023-11-10 10:05:08 +0800

    chore: revise dependencies and build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.ams.timesyncedualert"
-    compileSdk = 33
+    namespace = "com.ams.cspapplication"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.ams.timesyncedualert"
+        applicationId = "com.ams.cspapplication"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
@@ -47,15 +47,18 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.code.gson:gson:2.8.8")
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -63,4 +66,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
