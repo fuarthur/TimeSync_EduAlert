202c3c4dbbeab8e5de996e23546847fa94d84cc8
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2023-11-16 14:30:03 +0800

    feat: implement cover page

---
+++ app/src/main/java/com/ams/timesyncedualert/ui/HomepageActivity.kt
@@ -0,0 +1,12 @@
+package com.ams.timesyncedualert.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.ams.cspapplication.R
+
+class HomepageActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}
--- app/src/main/java/com/ams/timesyncedualert/ui/MainActivity.kt
+++ app/src/main/java/com/ams/timesyncedualert/ui/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.ams.timesyncedualert.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import com.ams.cspapplication.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cover)
+
+        Handler().postDelayed({
+            navigateToHomepage()
+        }, R.integer.cover_delay_milis.toLong())
+    }
+
+    private fun navigateToHomepage() {
+        val intent = Intent(this, HomepageActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
