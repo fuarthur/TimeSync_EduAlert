c23a2188b8804ba142e183a3fbba695ae506b92b
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2023-11-24 12:41:02 +0800

    style: reformat and add comment

--- app/src/main/java/com/ams/timesyncedualert/utils/FileHandler.kt
+++ app/src/main/java/com/ams/timesyncedualert/utils/FileHandler.kt
@@ -1,6 +1,5 @@
 package com.ams.timesyncedualert.utils
 
-import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.nio.charset.Charset
@@ -10,32 +9,39 @@ import java.io.File
 class FileHandler {
 
     object CourseHandler {
-        const val FILENAME = "/course_list.json"
+        // Filename for the course list
+        private const val FILENAME = "/course_list.json"
 
+        // Check if the file exists, create it if not, and initialize with an empty JSON array
         fun checkAndCreateFile(filePath: String) {
             val file = File(filePath + FILENAME)
+
             if (!file.exists()) {
                 file.createNewFile()
                 file.writeText("[]", Charset.defaultCharset())
             }
         }
 
+        // Read the course list from the specified file path
         fun readCourseList(filePath: String): MutableList<Course> {
-            val fileContent = java.io.File(filePath + FILENAME).readText(Charset.defaultCharset())
+            val fileContent = File(filePath + FILENAME).readText(Charset.defaultCharset())
             val listType = object : TypeToken<MutableList<Course>>() {}.type
             return Gson().fromJson(fileContent, listType)
         }
 
+        // Write the course list to the specified file path
         fun writeCourseList(courseList: MutableList<Course>, filePath: String) {
             val json = Gson().toJson(courseList)
-            java.io.File(filePath + FILENAME).writeText(json, Charset.defaultCharset())
+            File(filePath + FILENAME).writeText(json, Charset.defaultCharset())
         }
 
     }
 
     object SettingHandler {
-        const val FILENAME = "/setting.json"
+        // Filename for the settings
+        private const val FILENAME = "/setting.json"
 
+        // Check if the file exists, create it if not, and initialize with an empty JSON array
         fun checkAndCreateFile(filePath: String) {
             val file = File(filePath + FILENAME)
 
@@ -45,15 +51,17 @@ class FileHandler {
             }
         }
 
+        // Read the settings from the specified file path
         fun readCourseList(filePath: String): MutableList<Course> {
-            val fileContent = java.io.File(filePath + FILENAME).readText(Charset.defaultCharset())
+            val fileContent = File(filePath + FILENAME).readText(Charset.defaultCharset())
             val listType = object : TypeToken<MutableList<Course>>() {}.type
             return Gson().fromJson(fileContent, listType)
         }
 
+        // Write the settings to the specified file path
         fun writeCourseList(courseList: MutableList<Course>, filePath: String) {
             val json = Gson().toJson(courseList)
-            java.io.File(filePath + FILENAME).writeText(json, Charset.defaultCharset())
+            File(filePath + FILENAME).writeText(json, Charset.defaultCharset())
         }
     }
 }
