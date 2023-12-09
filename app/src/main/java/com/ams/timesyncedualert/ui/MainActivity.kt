111e324b01869a326c4c4053f70a3c15bc7c9abe
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2023-12-09 21:22:43 +0800

    chore: remove useless log

--- app/src/main/java/com/ams/timesyncedualert/ui/MainActivity.kt
+++ app/src/main/java/com/ams/timesyncedualert/ui/MainActivity.kt
@@ -14,22 +14,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cover)
-        Log.d("user", "enter cover")
 
         // Initialize data file
         val context: Context = this
         FileHandler.CourseHandler.checkAndCreateFile(context.filesDir.toString())
         FileHandler.SettingHandler.checkAndCreateFile(context.filesDir.toString())
-        Log.d("user", "23")
 
         Handler(Looper.myLooper()!!).postDelayed({
-            Log.d("user", "enter loop")
             navigateToHomepage()
         }, resources.getInteger(R.integer.cover_delay_milis).toLong())
     }
 
     private fun navigateToHomepage() {
-        Log.d("user", "enter homepage 0")
         val intent = Intent(this, HomepageActivity::class.java)
         startActivity(intent)
         finish()
