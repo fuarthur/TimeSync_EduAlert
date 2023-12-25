fa98e4743ecebbd7abf1fdb566dbd34aa14364e8
ARtcgb <141157002+fuarthur@users.noreply.github.com>
2023-12-25 22:55:46 +0800

    fix: #7

--- app/src/main/java/com/ams/timesyncedualert/ui/HomepageActivity.kt
+++ app/src/main/java/com/ams/timesyncedualert/ui/HomepageActivity.kt
@@ -3,7 +3,7 @@ package com.ams.timesyncedualert.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.ams.timesyncedualert.R
@@ -23,40 +23,40 @@ class HomepageActivity : AppCompatActivity() {
     private lateinit var timeTable: Map<String, List<Int>>
     private lateinit var courseList: MutableList<Course>
     private lateinit var bottomNavigation: BottomNavigationView
+    private var timer: Timer? = null
+    private val timePreset = 10 // Assuming this is a constant value
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
-        bottomNavigation = findViewById(R.id.bottom_navi)
-
-        bottomNavigation.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_Home -> {
-
-                }
-
-                R.id.navigation_Schedule -> {
-                    // 切换到 ScheduleFragment
-                    navigateToSchedule()
-                }
-
-                R.id.navigation_Setting -> {
-                    // 切换到 SettingFragment
-                    navigateToSetting()
-                }
-            }
-            when (item.itemId) {
-                R.id.navigation_Home -> {
-                    item.setIcon(if (item.isChecked) R.drawable.home_selected else R.drawable.home_idle)
-                }
-            }
-            true
-        }
+        setupBottomNavigation()
         getTimeTableAndCourseList()
         updateCurrentPeriod()
         updateUI()
         countDownTick()
+    }
+
+    private fun setupBottomNavigation() {
+        bottomNavigation = findViewById(R.id.bottom_navi)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_Home -> item.setIcon(
+                    getIconResource(
+                        item,
+                        R.drawable.home_selected,
+                        R.drawable.home_idle
+                    )
+                )
+                R.id.navigation_Schedule -> navigateToSchedule()
+                R.id.navigation_Setting -> navigateToSetting()
+            }
+            true
+        }
+    }
+
+    private fun getIconResource(item: MenuItem, selectedRes: Int, idleRes: Int): Int {
+        return if (item.isChecked) selectedRes else idleRes
     }
 
     private fun updateUI() {
@@ -70,57 +70,8 @@ class HomepageActivity : AppCompatActivity() {
     }
 
     private fun setNextPeriodText(view: TextView, course: Course?) {
-        view.text = if (course == null) {
-            "N/A"
-        } else {
-            formatCourseText(course)
-        }
+        view.text = if (course == null) "N/A" else formatCourseText(course)
     }
-
-    private fun countDownTick() {
-        val countdownValue = timeDiff()
-
-        if (countdownValue != -1) {
-            val timer = Timer()
-            val updateCountdownTask = object : TimerTask() {
-                var countdown = countdownValue
-
-                // val timePreset = FileHandler.SettingHandler.readSettings(context.filesDir.toString())[0].toString().toInt()
-                val timePreset = 10
-
-                override fun run() {
-                    runOnUiThread {
-                        val minutes = countdown - 1
-                        val displayText = "$minutes minutes"
-                        mCountdown.text = displayText
-
-                        if (minutes == timePreset) {
-                            val course = courseList[currentPeriod]
-                            val title = course.name + " at room " + course.classroom
-                            val content = "Your class " + course.name + " will begin in " + timePreset + " minutes."
-                            NotificationHelper.sendNotification(context, title, content)
-                        }
-                    }
-
-                    countdown-- // 每次执行任务，倒计时减1
-
-                    if (countdown <= 0) {
-                        timer.cancel() // todo: 倒计时结束后开始下一个任务
-                        getTimeTableAndCourseList()
-                        updateCurrentPeriod()
-                        updateUI()
-                        countDownTick()
-                    }
-                }
-            }
-
-            timer.scheduleAtFixedRate(updateCountdownTask, 0, 60 * 1000)
-        } else {
-            // 如果 timeDiff() 返回 -1，直接将 mCountdown.text 设置为 "N/A"
-            mCountdown.text = "N/A"
-        }
-    }
-
 
     private fun formatCourseText(course: Course): String {
         return "${course.name} at ${course.classroom}"
@@ -144,27 +95,21 @@ class HomepageActivity : AppCompatActivity() {
     }
 
     private fun navigateToSchedule() {
-        val intent = Intent(this, ScheduleActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, ScheduleActivity::class.java))
         finish()
     }
 
     private fun navigateToSetting() {
-        val intent = Intent(this, SettingActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this, SettingActivity::class.java))
         finish()
     }
 
     private fun timeDiff(): Int {
-
-        // 获取 Calendar 对象的实例
         val calendar = Calendar.getInstance()
 
-        // 通过 Calendar 对象获取当前的小时、分钟和秒
-        val hour = calendar.get(Calendar.HOUR_OF_DAY) // 获取小时，24小时制
-        val minute = calendar.get(Calendar.MINUTE)      // 获取分钟
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
 
-        // 构造时间字符串
         val currentTimeNum = hour * 60 + minute
         if (getNextCourseIfExist(currentPeriod) == null) {
             return -1
@@ -172,7 +117,48 @@ class HomepageActivity : AppCompatActivity() {
         val hourTime = timeTable[currentPeriod.toString()]!![0]
         val minuteTime = timeTable[currentPeriod.toString()]!![1]
         val nextTimeNum = hourTime * 60 + minuteTime
-        return currentTimeNum - nextTimeNum
+        return nextTimeNum - currentTimeNum
+    }
+
+    private fun countDownTick() {
+        val countdownValue = timeDiff()
+        if (countdownValue <= 0) {
+            mCountdown.text = "N/A"
+            return
+        }
+
+        timer = Timer().apply {
+            scheduleAtFixedRate(object : TimerTask() {
+                var countdown = countdownValue
+
+                override fun run() {
+                    runOnUiThread {
+                        updateCountdownDisplay(countdown)
+                        countdown--
+                        if (countdown <= 0) cancel()
+                    }
+                }
+            }, 0, 60 * 1000)
+        }
+    }
+
+    private fun updateCountdownDisplay(minutes: Int) {
+        val displayText = "$minutes minutes"
+        mCountdown.text = displayText
+        if (minutes == timePreset) sendNotification()
+    }
+
+    private fun sendNotification() {
+        val course = courseList.getOrNull(currentPeriod)
+        course?.let {
+            val title = "${it.name} at room ${it.classroom}"
+            val content = "Your class ${it.name} will begin in $timePreset minutes."
+            NotificationHelper.sendNotification(context, title, content)
+        }
+    }
+
+    private fun getNextCourseIfExist(tPeriod: Int): Course? {
+        return courseList.find { it.period == tPeriod }
     }
 
     private fun getTimeTableAndCourseList() {
@@ -187,14 +173,5 @@ class HomepageActivity : AppCompatActivity() {
             FileHandler.CourseHandler.readCourseList(context.filesDir.toString()),
             weekday
         )
-    }
-
-    private fun getNextCourseIfExist(tPeriod: Int): Course? {
-        for (course in courseList) {
-            if (course.period == tPeriod) {
-                return course
-            }
-        }
-        return null
     }
 }
